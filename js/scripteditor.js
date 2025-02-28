866f862c1d89f1c2340f5fcedf115ef66b5bc4ff
cambalachetechnologies <josefina.ceriani@c-tech.com.ar>
2025-02-27 21:53:55 -0300

    Update scripteditor.js

--- js/scripteditor.js
+++ js/scripteditor.js
@@ -1,38 +1,3 @@
-// Datos de usuarios (en un caso real, esto se almacenaría en una base de datos segura)
-const usuarios = {
-    "usuario1": "password123", // Usuario 1
-    "usuario2": "password456", // Usuario 2
-    "usuario3": "password789"  // Usuario 3
-};
-
-// Obtener elementos del DOM
-const loginForm = document.getElementById("loginForm");
-const loginSection = document.getElementById("loginSection");
-const notaFormSection = document.getElementById("notaFormSection");
-const errorMessage = document.getElementById("errorMessage");
-const notaForm = document.getElementById("notaForm");
-
-// Función para manejar el login
-function handleLogin(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de la manera tradicional
-
-    const username = document.getElementById("username").value;
-    const password = document.getElementById("password").value;
-
-    // Verificar las credenciales
-    if (usuarios[username] && usuarios[username] === password) {
-        // Si las credenciales son correctas, mostramos el editor de notas
-        loginSection.style.display = "none";
-        notaFormSection.style.display = "block";
-    } else {
-        // Si las credenciales son incorrectas, mostramos un mensaje de error
-        errorMessage.style.display = "block";
-    }
-}
-
-// Evento para manejar el submit del formulario de login
-loginForm.addEventListener("submit", handleLogin);
-
 // Función para manejar el formulario y enviar los datos al backend
 function handleNotaForm(event) {
     event.preventDefault(); // Prevenir el comportamiento por defecto
@@ -52,8 +17,8 @@ function handleNotaForm(event) {
         parrafo_2
     };
 
-    // Enviar los datos al backend
-    fetch('http://localhost:3000/saveNote', {
+    // Enviar los datos al backend de Netlify
+    fetch('/.netlify/functions/saveNote', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
